49a19cc448d0948158d42a216ba59633f2693e4f
Alan <liangrongjun@cosla.com>
2023-01-31 22:08:41 +0800

    打包文件上传

---
+++ qiniu-upload-prefix.js
@@ -0,0 +1,7 @@
+module.exports = {
+  prefixhost: 'https://static.aidianzan.com', // //七牛cdn绑定的域名
+  prefix: '', // // 七牛目录
+  accessKey: 'm0H-_ajIb8FF98p_T1qqfGmRtHgnYvgZc4Qi3RAK', //七牛accessKey
+  secretKey: 'xctWjtgzCeaa7uWhrnAHczxx8iQoWAyYmUXkQTxf', //七牛secretKey
+  space: 'aidianzan', //七牛空间名
+}
--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,12 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import { prefixhost, prefix } from './qiniu-upload-prefix';
 
 const path = require('path')
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // 注意 vite 默认是不加载 .env 文件的，因为这些文件需要在执行完 vite 配置后才能确定加载哪一个
-  // 使用 vite 导出的 loadEnv 函数来加载指定的 .env 文件
-  // 获取 vite 扫描到的环境变量，扫描文件按如下顺序
-  // .env.[model].local
-  // .env.[model]
-  // .env.local
-  // .env
   const env = loadEnv(mode, path.resolve(__dirname))
-
   return {
     plugins: [
       vue(),
@@ -40,5 +32,6 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    base: true ? prefixhost + prefix : '/'
   }
 })
